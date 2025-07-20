1f07bbfdfae623b1fc85cc6101669a8c4f9a5d64
rohan <rohanverma031@gmail.com>
2025-07-20 17:37:42 +0530

    feat: added client side navigation for react aria components and support for external links

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,20 +2,35 @@ import {
   Outlet,
   createRootRouteWithContext,
   useNavigate,
+  useRouter,
+  type ToPathOption,
+  type RegisteredRouter,
+  type NavigateOptions,
 } from "@tanstack/react-router";
 
 import type { QueryClient } from "@tanstack/react-query";
 import { Devtools } from "@/components/Devtools/Devtools";
 import { useEffect } from "react";
 import { Hub } from "aws-amplify/utils";
+import { RouterProvider } from "react-aria-components";
 
 interface MyRouterContext {
   queryClient: QueryClient;
 }
 
+// https://stackblitz.com/edit/tanstack-router-hhpjdc?file=src%2Froutes%2F__root.tsx&preset=node
+// used this for react-aria-components usage with tanstack-router
+declare module "react-aria-components" {
+  interface RouterConfig {
+    href: ToPathOption<RegisteredRouter, "/", "/"> | (string & {});
+    routerOptions: Omit<NavigateOptions, "to" | "from">;
+  }
+}
+
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => {
     const navigate = useNavigate();
+    const router = useRouter();
 
     useEffect(() => {
       const hubListenerCancellation = Hub.listen("auth", ({ payload }) => {
@@ -36,10 +51,40 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
     }, []);
 
     return (
-      <>
+      <RouterProvider
+        navigate={(to, options) =>
+          router.navigate({
+            ...options,
+            to: to as ToPathOption<RegisteredRouter, "/", "/">,
+          })
+        }
+        useHref={(to) => buildHref(to, router)}
+      >
         <Outlet />
         <Devtools />
-      </>
+      </RouterProvider>
     );
   },
 });
+
+// https://github.com/adobe/react-spectrum/issues/6397#issuecomment-2473017479
+// refrenced this for external url
+const ABSOLUTE_URL_PREFIXES = [
+  "https://",
+  "http://",
+  "mailto:",
+  "tel:",
+] as const;
+
+function buildHref(
+  to: ToPathOption<RegisteredRouter, "/", "/"> | (string & {}),
+  router: RegisteredRouter,
+): string {
+  if (!to) return "";
+
+  if (ABSOLUTE_URL_PREFIXES.some((prefix) => to.startsWith(prefix))) {
+    return to;
+  }
+
+  return router.buildLocation({ to }).href;
+}
--- src/routes/_homeLayout/index.tsx
+++ src/routes/_homeLayout/index.tsx
@@ -1,5 +1,6 @@
 import { ThemeSwitcher } from "@/components/ThemeSwitcher/ThemeSwitcher";
 import { createFileRoute } from "@tanstack/react-router";
+import { Link } from "@adgytec/adgytec-web-ui-components";
 
 export const Route = createFileRoute("/_homeLayout/")({
   component: RouteComponent,
@@ -9,6 +10,7 @@ function RouteComponent() {
   return (
     <div>
       <ThemeSwitcher />
+      <Link href="/login">Login</Link>
     </div>
   );
 }
